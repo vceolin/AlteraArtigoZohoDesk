77fc0618f574a107fd339b0ae000e7374bb8f896
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2020-12-09 09:40:33 -0300

    🦟 Correção de bug
    Correção de bug em que o botão era clicado antes do javascript da página ser carregado, quebrando o passo-a-passo.

--- AlteraArtigoZohoDesk/contentScript.js
+++ AlteraArtigoZohoDesk/contentScript.js
@@ -1,5 +1,6 @@
 const intervaloTempo = 1000; //Altere esse valor para mudar de quanto em quanto tempo o método retirarLabel será chamado
 console.log("############### Rodou!");
+setTimeout(clicarDropDown, intervaloTempo); //primeira instrução
 
 function clicarDropDown()
 {
@@ -7,6 +8,7 @@ function clicarDropDown()
 
     if(dropdown != null)
     {
+        console.log("############### DropDown encontrado!");
         dropdown.click();
         setTimeout(clicarEditHtml, intervaloTempo);
     }
@@ -21,12 +23,18 @@ function clicarEditHtml()
     var edit = document.querySelector('[data-insertoptions="edithtml"]');
     if(edit != null)
     {
+        console.log("############### Botão editar encontrado!");
         edit.click();
         setTimeout(executarReplace, intervaloTempo);
     }
-    else
+    else //se for nulo volta para o clicar dropDown
     {
-        setTimeout(clicarEditHtml, intervaloTempo);
+        /*
+        é necessário voltar para o clicar dropDown pois às vezes ele é encontrado
+        antes de ser totalmente carregado, então ao clicar nele não acontece nada e,
+        portanto, não carrega o botão "edit"
+        */
+        setTimeout(clicarDropDown, intervaloTempo);
     }
     
 }
@@ -34,18 +42,27 @@ function clicarEditHtml()
 function executarReplace()
 {
     document.getElementsByClassName("ze_ed_txtarea")[0].value = document.getElementsByClassName("ze_ed_txtarea")[0].value.replaceAll("suporte.groupsoftware.com.br", "desk.zoho.com");
+    console.log("############### Alteração realizada!");
     setTimeout(clicarSalvar, intervaloTempo);
 }
 
 function clicarSalvar()
 {
     document.getElementById("zdeditor_htmlview_insert").click();
+    console.log("############### Botão salvar clicado!");
     setTimeout(clicarPublicar, intervaloTempo);
 }
 
 function clicarPublicar()
 {
-    document.getElementById("publish_btn").click();
+    var publicar = document.getElementById("publish_btn")
+    if(publicar != null)
+    {
+        console.log("############### Botão publicar encontrado!");
+        publicar.click();
+    }
+    else //se for nulo tenta novamente em 1 segundo
+    {
+        setTimeout(clicarPublicar, intervaloTempo);
+    }
 }
-
-setTimeout(clicarDropDown, intervaloTempo);
