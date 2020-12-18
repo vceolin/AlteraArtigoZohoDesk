2686a136cea41af7476e63961a8a9f4f99c05bde
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2020-12-18 16:02:28 -0300

    🗺️ Changed POP up layout
    Changed button arrangment, preparing to activate/deactivate the extension based on user choice

--- AlteraArtigoZohoDesk/options.js
+++ AlteraArtigoZohoDesk/options.js
@@ -1,34 +1,14 @@
-// Saves options to chrome.storage
-function save_options() {
-    var tempoInst = document.getElementById('tempoInstrucao').value;
+// Salva a opção selecionada
+function salvarConfiguração(_opcaoSelecionada) {
+    var opcaoSelecionada = chrome.storage.opcaoSelecionada;
     chrome.storage.sync.set({
-        tempoInstrucao: tempoInst
+        opcaoSelecionada: _opcaoSelecionada
     }, function () {
-        // Update status to let user know options were saved.
+        // Mensagem para avisar que a alteração foi salva.
         var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
+        status.textContent = 'Salvo.';
         setTimeout(function () {
             status.textContent = '';
         }, 750);
     });
-}
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-    chrome.storage.sync.get({
-        tempoInstrucao: 1
-    }, function (items) {
-        document.getElementById('tempoInstrucao').value = items.tempoInstrucao;
-        document.getElementById('textInput').value = items.tempoInstrucao;
-    });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
-
-
-
-function updateTextInput(val) {
-    document.getElementById('textInput').value = val.text;
 }
