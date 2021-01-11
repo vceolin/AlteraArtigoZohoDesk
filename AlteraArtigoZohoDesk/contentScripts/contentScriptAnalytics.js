9eafcaa519215c4e82f3c78ddb356a8e818ec4fa
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2021-01-11 15:31:57 -0300

    🆕 Menu funcional!
    O pop up agora funciona, fazendo com que a extensão apenas execute o script que você selecionar!

--- AlteraArtigoZohoDesk/contentScripts/contentScriptAnalytics.js
+++ AlteraArtigoZohoDesk/contentScripts/contentScriptAnalytics.js
@@ -1,62 +1,70 @@
-console.log("############### Rodou!");
-
-//aguarda 5 segundos pra pagina iniciar completamente
-setTimeout(iniciarRestauracao, 5000);
-
-function iniciarRestauracao()
+//Pega a opção selecionada no pop up
+chrome.storage.sync.get('opcaoSelecionada', function (result) 
 {
-    //verifica se não possui a classe que diz que a lixeira está selecionada
-    if(!document.getElementsByClassName("trashNav")[0].classList.contains("navOptSelected"))
-    {
-        document.getElementsByClassName("trashNav")[0].click();
-        console.log("############### Clicou na lixeira!");
-    }
-    
-    //aguarda 5 segundos antes de rodar
-    setTimeout(marcarObjetos, 5000);
-}
+    opcao = result.opcaoSelecionada;
+});
 
-function marcarObjetos()
+//Só se executa esse script se a opção selecioanda for ArtigoDesk
+if(opcao == "LixeiraAnalytics")
 {
-    console.log("############### Iniciou a marcação!");
-    //pega todas as linhas da lista
-    var linhas = document.getElementById("TrashObjectList").getElementsByTagName("li");
-    //evento onMouseOver
-    const mouseoverEvent = new Event('mouseover');
+    //aguarda 5 segundos pra pagina iniciar completamente
+    setTimeout(iniciarRestauracao, 5000);
 
-    for(i = 0; i < linhas.length; i++)
+    function iniciarRestauracao()
     {
-        console.log("############### Navegando por linhas!");
-        //Primeira div, depois quinta div, depois primeiro span e pega o atributo tip
-        if(linhas[i].children[0].children[5].children[0].getAttribute("tip") == "Excluído por Sucesso Cliente")
+        //verifica se não possui a classe que diz que a lixeira está selecionada
+        if(!document.getElementsByClassName("trashNav")[0].classList.contains("navOptSelected"))
         {
-            console.log("############### Achou algo excluído pelo Sucesso Cliente!");
-            //primeiro o span acima do checkbox
-            var checkbox = linhas[i].children[0].children[0];
-            checkbox.style.opacity = 100;
-            checkbox.classList.add("fillcolor");
-            //agora o span do checkbox
-            checkbox.children[0].setAttribute("ischecked", true);
-            checkbox.children[0].classList.add("checkedBox");
+            document.getElementsByClassName("trashNav")[0].click();
+            console.log("############### Clicou na lixeira!");
         }
+        
+        //aguarda 5 segundos antes de rodar
+        setTimeout(marcarObjetos, 5000);
     }
 
-    concluiRestauracao();
-}
+    function marcarObjetos()
+    {
+        console.log("############### Iniciou a marcação!");
+        //pega todas as linhas da lista
+        var linhas = document.getElementById("TrashObjectList").getElementsByTagName("li");
+        //evento onMouseOver
+        const mouseoverEvent = new Event('mouseover');
 
-function concluiRestauracao()
-{
-    //aperta o botão de restaurar
-    document.getElementById("TrashViewRestoreButton").click();
-    console.log("############### Apertou o botão de restaurar!");
+        for(i = 0; i < linhas.length; i++)
+        {
+            console.log("############### Navegando por linhas!");
+            //Primeira div, depois quinta div, depois primeiro span e pega o atributo tip
+            if(linhas[i].children[0].children[5].children[0].getAttribute("tip") == "Excluído por Sucesso Cliente")
+            {
+                console.log("############### Achou algo excluído pelo Sucesso Cliente!");
+                //primeiro o span acima do checkbox
+                var checkbox = linhas[i].children[0].children[0];
+                checkbox.style.opacity = 100;
+                checkbox.classList.add("fillcolor");
+                //agora o span do checkbox
+                checkbox.children[0].setAttribute("ischecked", true);
+                checkbox.children[0].classList.add("checkedBox");
+            }
+        }
 
-//espera 10 minutos e da um refresh na pagina
-    setTimeout(function(){
-        chrome.tabs.getSelected(null, function(tab) {
-            var code = 'window.location.reload();';
-            chrome.tabs.executeScript(tab.id, {code: code});
-        });
-}, 100000);
+        concluiRestauracao();
+    }
+
+    function concluiRestauracao()
+    {
+        //aperta o botão de restaurar
+        document.getElementById("TrashViewRestoreButton").click();
+        console.log("############### Apertou o botão de restaurar!");
+
+    //espera 10 minutos e da um refresh na pagina
+        setTimeout(function(){
+            chrome.tabs.getSelected(null, function(tab) {
+                var code = 'window.location.reload();';
+                chrome.tabs.executeScript(tab.id, {code: code});
+            });
+        }, 100000);
+    }
 }
 
 
--- AlteraArtigoZohoDesk/popUp/options.js
+++ AlteraArtigoZohoDesk/popUp/options.js
@@ -1,14 +1,52 @@
-// Salva a opção selecionada
-function salvarConfiguração(_opcaoSelecionada) {
-    var opcaoSelecionada = chrome.storage.opcaoSelecionada;
-    chrome.storage.sync.set({
-        opcaoSelecionada: _opcaoSelecionada
-    }, function () {
-        // Mensagem para avisar que a alteração foi salva.
-        var status = document.getElementById('status');
-        status.textContent = 'Salvo.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 750);
+$(function()
+{
+    //constante para definir os nomes que serão salvos
+    const ids = ["LixeiraAnalytics", "ArtigoDesk", "Desativado"]
+    var opcao;
+
+    //pega o valor da opção selecionada no storage do chrome
+    chrome.storage.sync.get("opcaoSelecionada", function (result) 
+    {
+        opcao = result.opcaoSelecionada;
+        console.log(opcao);
     });
-}
+
+    //Roda a função de colorir o botão a primeira vez para recuperar a cor do selecionado antes
+    colorirBotao(opcao)
+
+    //botões
+    $("#btnLixeiraAnalytics").click(function()
+    {
+        colorirBotao(ids[0]);
+        salvarConfiguracao(ids[0]);
+    });
+    $("#btnArtigoDesk").click(function()
+    {
+        colorirBotao(ids[1]);
+        salvarConfiguracao(ids[1]);
+    });
+    $("#btnDesativado").click(function()
+    {
+        colorirBotao(ids[2]);
+        salvarConfiguracao(ids[2]);
+    });
+
+    // Colore o botão para aparecer como selecionado
+    function colorirBotao(_opcaoSelecionada) 
+    {
+        var idBotao = "#btn" + _opcaoSelecionada
+        //primeiro remove o selected de todos os botões
+        $("button").removeClass("active");
+        //agora coloca o selected no botão selecionado
+        $(idBotao).addClass("active");
+    }
+
+    // Salva a opção selecionada
+    function salvarConfiguracao(_opcaoSelecionada) 
+    {
+        var opcaoSelecionada = chrome.storage.opcaoSelecionada;
+        chrome.storage.sync.set({
+            opcaoSelecionada: _opcaoSelecionada
+        });
+    }
+})
